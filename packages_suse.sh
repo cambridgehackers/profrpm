60d6bc16ac6cddcc13e29fc6d71c2fffe3c6ba4d
John Ankcorn <jankcorn@gmail.com>
2012-08-04 20:09:49 -0400

    initial files

---
+++ packages_suse.sh
@@ -0,0 +1,3 @@
+#
+
+sudo yum install wget zlib-devel xz-devel nss-devel nspr-devel file-devel popt-devel cmake libcurl-devel libxml2-devel expat-devel openssl-devel boost-devel readline-devel augeas-devel bzip2-devel qemu-user glib2-devel
