3d6a06a3e8ba433f1a7ea9de2464f659c073f8c9
John Ankcorn <jankcorn@gmail.com>
2012-08-06 23:39:43 -0400

    updated again for fedora, but need to use 12.1 version here

--- packages_fedora.sh
+++ packages_fedora.sh
@@ -1,2 +1,2 @@
 #
-sudo yum install wget zlib-devel xz-devel nss-devel nspr-devel file-devel popt-devel cmake libcurl-devel libxml2-devel expat-devel openssl-devel boost-devel readline-devel augeas-devel bzip2-devel qemu-user glib2-devel
+sudo yum install wget zlib-devel xz-devel nss-devel nspr-devel file-devel popt-devel cmake libcurl-devel libxml2-devel expat-devel openssl-devel boost-devel readline-devel augeas-devel bzip2-devel qemu-user glib2-devel elfutils-libelf-devel perl-ExtUtils-Embed
