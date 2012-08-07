9c870978c51188259d3ece7e679e4f6843590387
John Ankcorn <jankcorn@gmail.com>
2012-08-06 23:56:50 -0400

    final fixes for gcc4.6 (using zypper 12.1)

--- build.sh
+++ build.sh
@@ -6,17 +6,19 @@ cd test/rpm-4.9.1.1
 make
 sudo make install
 sudo cp db.h /opt/profbuild/include/rpm/
-#needed by 11.4 verion of libzypp
-tar cf - lib/rpmdb_internal.h lib/backend/dbi.h lib/rpmhash.H | (cd /opt/profbuild/include/rpm/; sudo tar xf -)
 
-#cd ../libsolv
-cd ../sat-solver
-../../zmake.sh
+if [ "$GCC_VERSION" >= "4.6.0" ] ; then
+    cd ../libsolv
+    ../../zmake.sh
+else
+    #needed by 11.4 verion of libzypp
+    tar cf - lib/rpmdb_internal.h lib/backend/dbi.h lib/rpmhash.H | (cd /opt/profbuild/include/rpm/; sudo tar xf -)
+    cd ../sat-solver
+    ../../zmake.sh
+fi
 
 cd ../libzypp
 ../../zmake.sh
-#sudo cp cmake/modules/ZyppCommon.cmake /opt/profbuild/share/cmake/Modules/
-#sudo cp cmake/modules/FindZypp.cmake /opt/profbuild/share/cmake/Modules/
 
 cd ../zypper
 ../../zmake.sh
--- getsource.sh
+++ getsource.sh
@@ -1,4 +1,7 @@
 #
+set -e
+set -x
+GCC_VERSION=`gcc --version | head -1 | awk '{print $3}'`
 [ -e test ] || mkdir test
 cd test
 wget http://rpm.org/releases/rpm-4.9.x/rpm-4.9.1.1.tar.bz2
@@ -9,11 +12,14 @@ md5sum *
 # 6fd7cd6c4281e68fe9ec6644ce0fac6f  file-5.09.tar.gz
 # de64e7410721aa8e377adb8430cfc9e2  rpm-4.9.1.1.tar.bz2
 
-# c++ (GCC) 4.6.3 20120306 (Red Hat 4.6.3-2)
-VERSION=SuSE-Code-12_1-Branch
-# Ubuntu 10.04
-#c++ (Ubuntu 4.4.3-4ubuntu5.1) 4.4.3
-VERSION=SuSE-Code-11_4-Branch
+if [ "$GCC_VERSION" > "4.6.0" ] ; then
+    # c++ (GCC) 4.6.3 20120306 (Red Hat 4.6.3-2)
+    VERSION=SuSE-Code-12_1-Branch
+else
+    # Ubuntu 10.04
+    #c++ (Ubuntu 4.4.3-4ubuntu5.1) 4.4.3
+    VERSION=SuSE-Code-11_4-Branch
+fi
 git clone git://github.com/openSUSE/libzypp.git
 (cd libzypp; git checkout BASE-$VERSION)
 git clone git://github.com/openSUSE/zypper.git
