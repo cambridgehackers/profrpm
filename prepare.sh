8f27e462d375ff5ca4021c3c6ee5fe1dd5f52b1a
John Ankcorn <john.ankcorn@nokia.com>
2012-08-09 10:50:40 -0400

    added patches for removing c11 constructs from source code
    (allows compilation with gcc4.4 for ubuntu 10.04)

--- prepare.sh
+++ prepare.sh
@@ -7,8 +7,12 @@ tar xzf file-5.09.tar.gz
 tar xjf rpm-4.9.1.1.tar.bz2
 
 (cd libsolv; patch -p1 <../../libsolv_12_1.patch)
-(cd libzypp; patch -p2 <../../libzypp_12_1.patch)
-(cd zypper; patch -p2 <../../zypper_12_1.patch)
+(cd libzypp; patch -p1 <../../libzypp_12_1.patch)
+(cd zypper; patch -p1 <../../zypper_12_1.patch)
+
+#remove c11 extensions so that gcc4.4 can be used (for Ubuntu 10.04)
+(cd libzypp; patch -p1 <../../libzypp_12_1_no_c11.patch)
+(cd zypper; patch -p1 <../../zypper_12_1_no_c11.patch)
 
 cd rpm-4.9.1.1
 patch -p1 <../../rpm_chroot.patch 
