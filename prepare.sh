6f4ba350ec2cd2ad1822e92a139d6f7bedd717ea
John Ankcorn <john.ankcorn@nokia.com>
2012-08-09 08:49:58 -0400

    split patches for libsolv, libzypp and zypper

--- prepare.sh
+++ prepare.sh
@@ -6,6 +6,8 @@ tar xzf db-4.5.20.tar.gz
 tar xzf file-5.09.tar.gz
 tar xjf rpm-4.9.1.1.tar.bz2
 
+patch -p1 <../libsolv_12_1.patch 
+patch -p1 <../libzypp_12_1.patch 
 patch -p1 <../zypper_12_1.patch 
 
 cd rpm-4.9.1.1
