6d1dd73b32ba20a45d92241e5bfdfabb01fd4d6c
John Ankcorn <john.ankcorn@nokia.com>
2012-08-09 10:11:04 -0400

    get patch level correct (for old patches)

--- prepare.sh
+++ prepare.sh
@@ -6,9 +6,9 @@ tar xzf db-4.5.20.tar.gz
 tar xzf file-5.09.tar.gz
 tar xjf rpm-4.9.1.1.tar.bz2
 
-patch -p1 <../libsolv_12_1.patch 
-patch -p1 <../libzypp_12_1.patch 
-patch -p1 <../zypper_12_1.patch 
+(cd libsolv; patch -p1 <../../libsolv_12_1.patch)
+(cd libzypp; patch -p2 <../../libzypp_12_1.patch)
+(cd zypper; patch -p2 <../../zypper_12_1.patch)
 
 cd rpm-4.9.1.1
 patch -p1 <../../rpm_chroot.patch 
