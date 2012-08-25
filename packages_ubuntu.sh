a13c88c5e8e1a320e58176ad3282546f6dcc94af
John Ankcorn <jca@alum.mit.edu>
2012-08-25 08:50:02 -0400

    installed on ubuntu 11.04 system

--- packages_ubuntu.sh
+++ packages_ubuntu.sh
@@ -1,2 +1,2 @@
 #
-sudo apt-get install libmagic-dev libnss3-dev libnspr4-dev doxygen libboost-all-dev libproxy-dev libaugeas-dev
+sudo apt-get install libmagic-dev libnss3-dev libnspr4-dev doxygen libboost-all-dev libproxy-dev libaugeas-dev libpopt-dev cmake libelf-dev libbz2-dev curl libcurl4-openssl-dev libreadline6-dev libudev-dev
